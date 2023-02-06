d193abb9355f85affafb8459c5c5c472dae38442
Mekuzd <emekaseun.es@gmail.com>
2023-02-06 13:59:18 +0100

    remobed btn from app.jsx and also add fucn

--- client-side/src/App.jsx
+++ client-side/src/App.jsx
@@ -1,32 +1,7 @@
-import { useState } from "react";
 import DefaultLayout from "./Layouts/DefaultLayouts";
-import httpClient from "./Services/httpClient";
 
 function App() {
-  const [apiMes, setApimes] = useState("");
-  let calc = {
-    Fn: 20,
-    Sn: 3,
-    operator: "-",
-  };
-  async function add() {
-    try {
-      let response = await httpClient.post("/calc", calc);
-      console.log(response);
-      setApimes(response.data.message);
-    } catch (error) {
-      setApimes(error.response.data.message);
-    }
-  }
-
-  return (
-    <DefaultLayout>
-      <button className="btn btn-primary m-2" onClick={add}>
-        click me
-      </button>
-      <h1>{apiMes}</h1>
-    </DefaultLayout>
-  );
+  return <DefaultLayout></DefaultLayout>;
 }
 
 export default App;
